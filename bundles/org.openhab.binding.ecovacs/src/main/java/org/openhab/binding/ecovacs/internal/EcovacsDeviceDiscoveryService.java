52ad64e80cfc96eb95f2dfa785cb7e278425db91
Danny Baumann <dannybaumann@web.de>
2022-01-03 15:48:08 +0100

    [ecovacs] Discovery updates
    - Enable 'scan' button in UI
    - Adapt to API changes
    - Fetch product list with every rescan

--- bundles/org.openhab.binding.ecovacs/src/main/java/org/openhab/binding/ecovacs/internal/EcovacsDeviceDiscoveryService.java
+++ bundles/org.openhab.binding.ecovacs/src/main/java/org/openhab/binding/ecovacs/internal/EcovacsDeviceDiscoveryService.java
@@ -12,6 +12,7 @@
  */
 package org.openhab.binding.ecovacs.internal;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
@@ -21,29 +22,32 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.config.discovery.AbstractDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
+import org.openhab.core.config.discovery.DiscoveryService;
+import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingUID;
 import org.openhab.core.thing.binding.ThingHandler;
 import org.openhab.core.thing.binding.ThingHandlerService;
+import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.pott.sucks.api.EcovacsApi;
+import dev.pott.sucks.api.EcovacsApiException;
 import dev.pott.sucks.api.dto.response.portal.Device;
 import dev.pott.sucks.api.dto.response.portal.IotProduct;
-import dev.pott.sucks.api.dto.response.portal.PortalLoginResponse;
 
 @NonNullByDefault
+@Component(service = DiscoveryService.class, configurationPid = "discovery.ecovacs")
 public class EcovacsDeviceDiscoveryService extends AbstractDiscoveryService implements ThingHandlerService {
     private final Logger logger = LoggerFactory.getLogger(EcovacsDeviceDiscoveryService.class);
 
-    private static final int DISCOVER_TIMEOUT_SECONDS = 5;
+    private static final int DISCOVER_TIMEOUT_SECONDS = 10;
 
     private @NonNullByDefault({}) EcovacsApiHandler apiHandler;
-    private @Nullable List<IotProduct> products;
     private @Nullable Future<?> scanFuture;
 
     public EcovacsDeviceDiscoveryService() {
-        super(DISCOVER_TIMEOUT_SECONDS);
+        super(Collections.singleton(EcovacsBindingConstants.THING_TYPE_VACUUM), DISCOVER_TIMEOUT_SECONDS, true);
     }
 
     @Override
@@ -79,16 +83,35 @@ public class EcovacsDeviceDiscoveryService extends AbstractDiscoveryService impl
         logger.debug("Starting Ecovacs discovery scan");
         scanFuture = scheduler.submit(() -> {
             EcovacsApi api = apiHandler.getApi();
-            PortalLoginResponse loginData = apiHandler.getLoginData();
-            if (api == null || loginData == null) {
+            if (api == null) {
                 return;
             }
 
-            if (products == null) {
-                products = api.getIotProductMap(loginData).getProducts();
-            }
-            for (Device device : api.getDevices(loginData).getDevices()) {
-                deviceDiscovered(device);
+            try {
+                List<IotProduct> products = api.getIotProductMap();
+                List<Device> devices = api.getDevices();
+
+                for (Device device : devices) {
+                    Optional<IotProduct> product = products.stream()
+                            .filter(prod -> prod.getClassId().equals(device.getDeviceClass())).findFirst();
+                    if (!product.isPresent()) {
+                        logger.debug("Device {} has unknown class {}, ignoring.", device.getDid(),
+                                device.getDeviceClass());
+                        continue;
+                    }
+                    deviceDiscovered(device, product.get());
+                }
+                for (Thing thing : apiHandler.getThing().getThings()) {
+                    String deviceId = thing.getUID().getId();
+                    if (!devices.stream().anyMatch(d -> deviceId.equals(d.getDid()))) {
+                        thingRemoved(thing.getUID());
+                    }
+                }
+            } catch (EcovacsApiException e) {
+                logger.debug("Could not retrieve devices from Ecovacs API", e);
+            } finally {
+                removeOlderResults(getTimestampOfLastScan());
+                scanFuture = null;
             }
         });
     }
@@ -103,22 +126,15 @@ public class EcovacsDeviceDiscoveryService extends AbstractDiscoveryService impl
         super.stopScan();
     }
 
-    private void deviceDiscovered(Device device) {
-        Optional<IotProduct> product = products.stream()
-                .filter(prod -> prod.getClassId().equals(device.getDeviceClass())).findFirst();
-        if (!product.isPresent()) {
-            logger.debug("Device {} has unknown class {}, ignoring.", device.getDid(), device.getDeviceClass());
-            return;
-        }
-
+    private void deviceDiscovered(Device device, IotProduct product) {
         // TODO: check whether device actually is a vacuum cleaner - how?
 
         ThingUID thingUID = new ThingUID(EcovacsBindingConstants.THING_TYPE_VACUUM, apiHandler.getThing().getUID(),
                 device.getDid());
         DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID)
-                .withBridge(apiHandler.getThing().getUID()).withProperty("id", device.getDid())
-                .withProperty("serial", device.getName()).withProperty("type", product.get().getDefinition().name)
-                .withRepresentationProperty("type").build();
+                .withBridge(apiHandler.getThing().getUID()).withProperty(Thing.PROPERTY_SERIAL_NUMBER, device.getName())
+                .withProperty(Thing.PROPERTY_MODEL_ID, product.getDefinition().name)
+                .withRepresentationProperty(Thing.PROPERTY_MODEL_ID).build();
         thingDiscovered(discoveryResult);
     }
 }
