c7163e92a1704d2aeebee52c7c19c5412d822e3f
Danny Baumann <dannybaumann@web.de>
2022-01-16 15:09:42 +0100

    [ecovacs] Rename channels for consistency, add TODOs

--- bundles/org.openhab.binding.ecovacs/src/main/java/org/openhab/binding/ecovacs/internal/EcovacsBindingConstants.java
+++ bundles/org.openhab.binding.ecovacs/src/main/java/org/openhab/binding/ecovacs/internal/EcovacsBindingConstants.java
@@ -33,9 +33,9 @@ public class EcovacsBindingConstants {
     public static final String CHANNEL_ID_STATE = "state";
     public static final String CHANNEL_ID_COMMAND = "command";
     public static final String CHANNEL_ID_BATTERY_LEVEL = "battery";
-    public static final String CHANNEL_ID_CLEANING_TIME = "cleaningTime";
-    public static final String CHANNEL_ID_CLEANED_AREA = "cleanedArea";
-    public static final String CHANNEL_ID_WATER_PLATE_PRESENT = "waterPlatePresent";
+    public static final String CHANNEL_ID_CLEANING_TIME = "cleaning-time";
+    public static final String CHANNEL_ID_CLEANED_AREA = "cleaned-area";
+    public static final String CHANNEL_ID_WATER_PLATE_PRESENT = "water-system-present";
 
     public static final String CMD_AUTO_CLEAN = "clean";
     public static final String CMD_PAUSE = "pause";
