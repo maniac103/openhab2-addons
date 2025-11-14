b0511c4fe456226ecccfdecd2e3b7434db4a337e
Danny Baumann <dannybaumann@web.de>
2025-11-14 15:26:54 +0100

    [ecoflow] Add initial support for STREAM home solar system
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- bundles/org.openhab.binding.ecoflow/src/main/java/org/openhab/binding/ecoflow/internal/discovery/EcoflowDeviceDiscoveryService.java
+++ bundles/org.openhab.binding.ecoflow/src/main/java/org/openhab/binding/ecoflow/internal/discovery/EcoflowDeviceDiscoveryService.java
@@ -12,9 +12,7 @@
  */
 package org.openhab.binding.ecoflow.internal.discovery;
 
-import static org.openhab.binding.ecoflow.internal.EcoflowBindingConstants.THING_TYPE_DELTA2;
-import static org.openhab.binding.ecoflow.internal.EcoflowBindingConstants.THING_TYPE_DELTA2MAX;
-import static org.openhab.binding.ecoflow.internal.EcoflowBindingConstants.THING_TYPE_POWERSTREAM;
+import static org.openhab.binding.ecoflow.internal.EcoflowBindingConstants.*;
 
 import java.time.Instant;
 import java.util.List;
@@ -126,6 +124,9 @@ public class EcoflowDeviceDiscoveryService extends AbstractThingHandlerDiscovery
             case "DELTA 2" -> THING_TYPE_DELTA2;
             case "DELTA 2 Max" -> THING_TYPE_DELTA2MAX;
             case "PowerStream" -> THING_TYPE_POWERSTREAM;
+            case "STREAM AC" -> THING_TYPE_STREAM_AC;
+            case "STREAM Max" -> THING_TYPE_STREAM_MAX;
+            case "STREAM Pro", "STREAM AC Pro", "STREAM Ultra", "STREAM Ultra X" -> THING_TYPE_STREAM_PRO_ULTRA;
             default -> null;
         };
         if (thingTypeUID == null) {
